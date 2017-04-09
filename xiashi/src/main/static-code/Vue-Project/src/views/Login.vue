9f227f52130b6399c0c336219a4ae861340837ee
d37893545 <378935454@qq.com>
2017-04-09 21:04:57 +0800

    sql

--- xiashi/src/main/static-code/Vue-Project/src/views/Login.vue
+++ xiashi/src/main/static-code/Vue-Project/src/views/Login.vue
@@ -1,16 +1,17 @@
-<template >
-  <el-form  :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px"
+<template>
+  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px"
            class="demo-ruleForm login-container">
     <h3 class="title">系统登录</h3>
     <el-form-item prop="account">
       <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
     </el-form-item>
     <el-form-item prop="checkPass">
-      <el-input type="password"  v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
+      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="密码"></el-input>
     </el-form-item>
     <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
     <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;"  @click.native.prevent="handleSubmit2" :loading="logining">登录
+      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录
+
       </el-button>
       <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
     </el-form-item>
@@ -18,7 +19,7 @@
 </template>
 
 <script>
-  import { requestLogin } from '../api/api'
+  import {requestLogin} from '../api/api'
   // import NProgress from 'nprogress'
   export default {
     data () {
@@ -30,11 +31,11 @@
         },
         rules2: {
           account: [
-            {required: true, message: '请输入账号', trigger: 'blur'}
+            { required: true, message: '请输入账号', trigger: 'blur' }
             // { validator: validaePass }
           ],
           checkPass: [
-            {required: true, message: '请输入密码', trigger: 'blur'}
+            { required: true, message: '请输入密码', trigger: 'blur' }
             // { validator: validaePass2 }
           ]
         },
@@ -52,26 +53,29 @@
             // _this.$router.replace('/table');
             $this.logining = true
             // NProgress.start();
-            let loginParams = {accountName: $this.ruleForm2.account, password: $this.ruleForm2.checkPass}
+            let loginParams = { accountName: $this.ruleForm2.account, password: $this.ruleForm2.checkPass }
             $this.http.post('/mobile/check-login', qs.stringify(loginParams))
               .then(response => {
-              $this.logining = false
-              // NProgress.done();
-              let {msg, code, data} = response.data
-              if (code !== 1) {
+                $this.logining = false
+                // NProgress.done();
+                let { msg, code, data } = response.data
+                if (code !== 1) {
+                  $this.$message({
+                    message: msg,
+                    type: 'error'
+                  })
+                } else {
+                  sessionStorage.setItem('user', JSON.stringify(data))
+                  $this.$router.push({ path: '/main' })
+                }
+              }).catch(error => {
+                console.log(error)
                 $this.$message({
-                  message: msg,
+                  message: error,
                   type: 'error'
                 })
-              } else {
-                sessionStorage.setItem('user', JSON.stringify(data))
-                $this.$router.push({path: '/main'})
               }
-            })
-              .catch(error=>{
-                  console.log(error)
-                }
-              )
+            )
 
           } else {
             console.log('error submit!!')
@@ -98,6 +102,7 @@
     border: 1px solid #eaeaea;
     box-shadow: 0 0 25px #cac6c6;
   }
+
   .title {
     margin: 0px auto 40px auto;
     text-align: center;
