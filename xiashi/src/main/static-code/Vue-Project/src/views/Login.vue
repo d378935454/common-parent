2bbb8d9e2eb4e00deac721b116e30c2ac94837a6
duhongda <378935454@qq.com>
2017-04-18 14:44:57 +0800

    登录页面

--- xiashi/src/main/static-code/Vue-Project/src/views/Login.vue
+++ xiashi/src/main/static-code/Vue-Project/src/views/Login.vue
@@ -1,42 +1,21 @@
 <template>
-  <div>
-    <div class="login-header">
-      <p>登录ERP系统</p>
-      <div class="logo"></div>
-    </div>
-    <div class="box-size login-form">
-      <span class="fr login-btn"></span>
-      <h1>系统登录</h1>
-      <form class="login-form-list" id="form-id" action="">
-        <ul class="login-ul">
-          <li class="box-size">
-            <div class="box-size">
-              <i class="iconfont icon1">&#xe610;</i>
-              <input id="phone" name="phone" class="field-text box-size f-input" type="tel" datatype="m" placeholder="手机号码"  >
-              <button type="button" class="blue-color getyzm" autocomplete="off">获取验证码</button>
-            </div>
-          </li>
-          <li class="box-size">
-            <div>
-              <i class="iconfont icon1">&#xe611;</i>
-              <input id="password" name="password" class="field-text box-size f-input" type="password" placeholder="密码" >
-            </div>
-          </li>
-          <li class="box-size">
-            <div>
-              <i class="iconfont icon1">&#xe60b;</i>
-              <input id="code" name="code" class="field-text box-size f-input" type="number" placeholder="验证码" >
-            </div>
-          </li>
-          <li class="box-size" style="height:0.58rem;padding-top:0;padding-bottom:0">
-            <div>
-              <input class="box-size blue-color u-tj_btn" type="submit" value="登录" autocomplete="off" style="padding:0;" />
-            </div>
-          </li>
-        </ul>
+    <div class="login-body">
+      <header class="login-header">
+        <b>欢迎登录</b>
+      </header>
+      <form class="form">
+        <div class="form-group">
+          <label for="user">用户名</label>
+          <input type="email" class="form-control" id="user" placeholder="请输入用户名">
+        </div>
+        <div class="form-group">
+          <label for="pswd">密码</label>
+          <input type="password" class="form-control" id="pswd" placeholder="请输入密码">
+        </div>
+        <button type="button" @click="handleSubmit2" class="btn btn-primary cursor ">登录</button>
       </form>
     </div>
-  </div>
+
 </template>
 
 <script>
@@ -95,155 +74,66 @@
 
 </script>
 
-<style >
+<style>
+  /* ==============初始化样式============== */
+  *{box-sizing:border-box;-moz-box-sizing:border-box; -webkit-box-sizing:border-box; outline: none;}
+  body,h1,h2,h3,h4,h5,h6,hr,p,blockquote,dl,dt,dd,ul,ol,li,pre,form,fieldset,legend,button,input,textarea,th,td { margin: 0; padding: 0; border: 0;}
+  html,body { height: 100%; }
+  body,button,input,select,textarea { font-size: 12px; font-family: 'Microsoft Yahei','微软雅黑', \5FAE\8F6F\96C5\9ED1,'宋体',\5b8b\4f53,arial,'Hiragino Sans GB',Tahoma,Arial,Helvetica; }
+  ul,ol,li,dl,dd,dt{list-style: none;}
+  em { font-style: normal; }
+  a { text-decoration: none; }
+  a:hover { text-decoration: none; }
+  legend { color: #000; }
+  fieldset,img { border: 0; }
+  label,button { cursor: pointer; }
+  /* dfa */
+  html,body,form,fieldset,p,div,h1,h2,h3,h4,h5,h6{-webkit-text-size-adjust:none;}
+  article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block;}
+  input[type="submit"],input[type="reset"],input[type="button"],button,input[type="search"],select,input[type=number],input[type=number]{-webkit-appearance: none;}
+  input[type='number'],input[type='tel'] {-moz-appearance:textfield;}
+  input[type=number]::-webkit-inner-spin-button,input[type=number]::-webkit-outer-spin-button {-webkit-appearance: none ;margin: 0;}{-moz-appearance:textfield;}
+  input[type=number]::-webkit-inner-spin-button,input[type=number]::-webkit-outer-spin-button {-webkit-appearance: none ;margin: 0;}
+  /*解决IE图片缩放失真*/
+  img { -ms-interpolation-mode: bicubic; }
+  /*清除浮动*/
+  .clearfix:after { content: ''; display: block; height: 0; clear: both; }
+  .clearfix { zoom: 1; line-height: normal; }
+  .clear { clear: both; }
+  /*连续英文、数字换行*/
+  .wordwrap { word-break: break-all; word-wrap: break-word; }
+  /*单行文字超出显示省略号*/
+  .omg{overflow: hidden; text-overflow: ellipsis; white-space:nowrap;}
+  /*ios去除点击时的灰色遮罩*/
+  a,button,input,textarea,select{-webkit-tap-highlight-color: rgba(255,255,255,0);}
+  *{
+    -webkit-touch-callout:none;  /*系统默认菜单被禁用*/
+    -webkit-user-select:none; /*webkit浏览器*/
+    user-select:none;
+  }
+  input{
+    -webkit-user-select:auto; /*webkit浏览器*/
+  }
+
   .login-header {
     width: 100%;
     text-align: center;
+    padding:20% 0 10%;
   }
 
-  .login-header p {
+  .login-header b {
     width: 100%;
-    font-size: 0.24rem;
-    color: #fff;
+    font-size: 16px;
     text-align: center;
   }
 
-  .logo {
-    display: inline-block;
-    margin-top: 0.38rem;
-    width: 3.67rem;
-    height: 0.68rem;
-    /*background: url('../images/icon/logo.png') 0 0 no-repeat;*/
-    -webkit-background-size: 100% 100%;
-    background-size: 100% 100%;
-  }
 
-  .login-form {
-    margin: 0.6rem auto 0;
-    width: 4.45rem;
-    height: 4.95rem;
-    border-radius: 0.05rem;
-    border: 1px solid #2f2f2f;
-    padding: 0.15rem 0.27rem;
-    /*background: url('../images/img/loginformbg.gif') 0 0 repeat;*/
-  }
 
-  .login-form span.login-btn {
-    display: block;
-    width: 0.24rem;
-    height: 0.24rem;
-    /*background: url('../images/icon/login_btn.png') 0 0 no-repeat;*/
-    -webkit-background-size: 100% 100%;
-    background-size: 100% 100%;
-  }
 
-  .login-form h1 {
-    margin-top: 0.15rem;
-    font-size: 0.3rem;
-    color: #fff;
-    text-align: left;
-  }
-
-  .login-form-list {
-    margin-top: 0.38rem;
-    width: 3.9rem;
-  }
-
-  .login-ul {
+.form{
+  margin: 0 10%;
+}
+  .btn-primary{
     width: 100%;
-  }
-
-  .login-ul li {
-    margin-bottom: 0.18rem;
-    width: 100%;
-    border: 1px solid #353535;
-    border-radius: 0.03rem;
-    background-color: #1B1B1B;
-  }
-
-  .login-ul li div {
-    width: 100%;
-    position: relative;
-  }
-
-  .login-ul li div i.icon1 {
-    position: absolute;
-    top: 50%;
-    left: 0.1rem;
-    transform: translateY(-50%);
-    color: #AAAAAA;
-    font-size: 0.24rem;
-  }
-
-  .login-ul li:first-child div input {
-    padding-right: 1.4rem;
-  }
-
-  .login-ul li div input {
-    padding: 0.18rem 0 0.18rem 0.46rem;
-    width: 100%;
-    background: none;
-    font-size: 0.18rem;
-    color: #fff;
-    outline: none;
-  }
-
-  .login-ul li:last-child {
-    margin-top: 0.42rem;
-  }
-
-  .login-ul li:last-child div input {
-    padding-left: 0;
-    border-radius: 3px;
-    background-color: #4c88f5;
-    font-size: 0.18rem;
-    color: #fff;
-    line-height: 0.58rem;
-    text-align: center;
-  }
-
-  .login-ul li div button {
-    position: absolute;
-    top: 50%;
-    right: 0.02rem;
-    padding: 0.02rem 0.14rem;
-    transform: translateY(-50%);
-    border-radius: 0.03rem;
-    background-color: #4c88f5;
-    font-size: 0.18rem;
-    color: #fff;
-    line-height: 0.34rem;
-    text-align: center;
-  }
-
-  .login-ul li div button.disabled {
-    background-color: #4E4E4E;
-  }
-
-
-
-
-  .login-container {
-    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
-    -webkit-border-radius: 5px;
-    border-radius: 5px;
-    -moz-border-radius: 5px;
-    background-clip: padding-box;
-    margin: 180px auto;
-    width: 100%;
-    padding: 35px 35px 15px 35px;
-    background: #fff;
-    border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
-  }
-
-  .title {
-    margin: 0px auto 40px auto;
-    text-align: center;
-    color: #505458;
-  }
-
-  .remember {
-    margin: 0px 0px 35px 0px;
   }
 </style>
